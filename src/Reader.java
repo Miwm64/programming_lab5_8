0e9e2a589c1b8f76ec6cc9e0400ac60cd16bc22e
Miwm64 <mike.dobkes@gmail.com>
2026-02-14 15:32:21 +0300

    Readers implemented

--- src/Reader.java
+++ src/Reader.java
@@ -1,5 +1,7 @@
+import java.io.IOException;
+
 public interface Reader {
-    String read();
-    String readNextLine();
-    boolean hasNextLine();
+    String read() throws IOException;
+    String readNextLine() throws IOException;
+    boolean hasNextLine() throws IOException;
 }
