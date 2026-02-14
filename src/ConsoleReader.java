1c60a62ce5099e6d4a9dc2151cbe9ecb22e60fc7
Miwm64 <mike.dobkes@gmail.com>
2026-02-14 14:55:42 +0300

    Console Reader

--- src/ConsoleReader.java
+++ src/ConsoleReader.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
-public class ConsoleReader {
+public class ConsoleReader implements Reader{
+    Scanner scanner = new Scanner(System.in);
+
+    @Override
+    public String read() {
+        return scanner.nextLine();
+    }
+
+    @Override
+    public String readNextLine() {
+        return scanner.nextLine();
+    }
+
+    @Override
+    public boolean hasNextLine() {
+        return true;
+    }
 }
