c5909c0df03a734761b5a34c6f8e0426b2bbbb84
Miwm64 <mike.dobkes@gmail.com>
2026-02-23 14:13:52 +0300

    Command getParams - return type changed to array

--- src/main/java/ru/spb/miwm64/moviemanager/command/Command.java
+++ src/main/java/ru/spb/miwm64/moviemanager/command/Command.java
@@ -3,7 +3,7 @@ package ru.spb.miwm64.moviemanager.command;
 import java.util.ArrayList;
 
 public interface Command {
-    Parameter<?> getParams();
+    ArrayList<Parameter<?>> getParams();
     void setParam(Parameter<?> param);
     void setParams(ArrayList<Parameter<?>> params);
     CommandResult execute();
