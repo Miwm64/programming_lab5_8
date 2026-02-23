821825ac11a44b482ded2418e3634d1fe419db60
Miwm64 <mike.dobkes@gmail.com>
2026-02-23 14:21:46 +0300

    Command getParams - expanded to 3 methods

--- src/main/java/ru/spb/miwm64/moviemanager/command/Command.java
+++ src/main/java/ru/spb/miwm64/moviemanager/command/Command.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 
 public interface Command {
     ArrayList<Parameter<?>> getParams();
+    ArrayList<Parameter<?>> getMissingParams();
+    ArrayList<Parameter<?>> getRemainingRequiredParams();
     void setParam(Parameter<?> param);
     void setParams(ArrayList<Parameter<?>> params);
     CommandResult execute();
