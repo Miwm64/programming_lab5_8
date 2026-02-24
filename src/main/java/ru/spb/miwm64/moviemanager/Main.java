2f517af2f5608c2c328655a34d3647ba6a8fe31b
Miwm64 <mike.dobkes@gmail.com>
2026-02-24 23:33:01 +0300

    Simple console logic

--- src/main/java/ru/spb/miwm64/moviemanager/Main.java
+++ src/main/java/ru/spb/miwm64/moviemanager/Main.java
@@ -1,9 +1,19 @@
 package ru.spb.miwm64.moviemanager;
 
-import ru.spb.miwm64.moviemanager.io.*;
+import ru.spb.miwm64.moviemanager.collectionmanager.CollectionManager;
+import ru.spb.miwm64.moviemanager.collectionmanager.SortedCollectionManager;
+import ru.spb.miwm64.moviemanager.io.ConsoleReader;
+import ru.spb.miwm64.moviemanager.io.ConsoleWriter;
+import ru.spb.miwm64.moviemanager.io.Reader;
+import ru.spb.miwm64.moviemanager.io.Writer;
 
 public class Main {
     public static void main(String[] args) {
+        CollectionManager collectionManager = new SortedCollectionManager();
+        Reader reader = new ConsoleReader();
+        Writer writer = new ConsoleWriter();
+        var mainController = new MainController(collectionManager, reader, writer);
+        mainController.run();
         return;
     }
 }
--- src/main/java/ru/spb/miwm64/moviemanager/MainController.java
+++ src/main/java/ru/spb/miwm64/moviemanager/MainController.java
@@ -1,16 +1,26 @@
 package ru.spb.miwm64.moviemanager;
 
 import ru.spb.miwm64.moviemanager.collectionmanager.CollectionManager;
+import ru.spb.miwm64.moviemanager.command.Command;
+import ru.spb.miwm64.moviemanager.command.CommandResult;
+import ru.spb.miwm64.moviemanager.commands.AddCommand;
+import ru.spb.miwm64.moviemanager.commands.HelpCommand;
+import ru.spb.miwm64.moviemanager.commands.ShowCommand;
+import ru.spb.miwm64.moviemanager.exceptions.NonExistentCommand;
 import ru.spb.miwm64.moviemanager.io.BufferedFileReader;
 import ru.spb.miwm64.moviemanager.io.Reader;
 import ru.spb.miwm64.moviemanager.io.Writer;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public final class MainController {
     private CollectionManager collectionManager;
     private List<Reader> readers;
+    private Reader defaultReader;
+    private Writer defaultWriter;
     private Writer writer;
     // XML parser
 
@@ -20,25 +30,94 @@ public final class MainController {
         this.readers = new ArrayList<>();
         readers.add(defaultReader);
         this.writer = defaultWriter;
+        this.defaultReader = defaultReader;
+        this.defaultWriter = defaultWriter;
     }
 
     public void run() {
-        while (true) {
-            boolean result;
-            if (readers.get(0) instanceof BufferedFileReader) {
-                result = fileRun();
-            } else {
-                result = consoleRun();
-            }
+        try {
+            while (true) {
+                boolean result;
+                if (readers.get(0) instanceof BufferedFileReader) {
+                    result = fileRun();
+                } else {
+                    result = consoleRun();
+                }
 
-            if (result) {
-                break;
+                if (result) {
+                    break;
+                }
             }
+        }
+        catch (Exception e) {
+            return;
         }
         // save xml
     }
 
-    private boolean consoleRun() {
+    private boolean consoleRun() throws IOException {
+        try {
+            writer.writeln("Enter command:");
+            String input = readers.get(0).readNextLine();
+            Command cmd;
+
+            ArrayList<String> inputs = new ArrayList<>(Arrays.asList(input.trim().split(" ")));
+            switch (inputs.get(0)){
+                case "help": {
+                    cmd = new HelpCommand();
+                    break;
+                }
+                case "show": {
+                    cmd = new ShowCommand(collectionManager);
+                    break;
+                }
+                case "add": {
+                    cmd = new AddCommand(collectionManager);
+                    break;
+                }
+                case "exit": {
+                    return true;
+                }
+                case null, default: {
+                    throw new NonExistentCommand("Command \"" + inputs.get(0) + "\" does not exist");
+                }
+            }
+
+            var params = cmd.getParams();
+
+            if (params.size() == 1 && inputs.size() >= 2) {
+                params.get(0).fromString(inputs.get(1));
+            }
+            else {
+                int i = 0;
+                while (i != params.size()) {
+                    try {
+                        var param = params.get(i);
+                        writer.writeln(param.getPrompt() + ":");
+                        input = readers.get(0).readNextLine();
+                        param.fromString(input);
+                        cmd.setParam(param);
+                        ++i;
+                    }
+                    catch (Exception e){
+                        writer.writeln("error: " + e.getMessage());
+                    }
+                }
+            }
+            CommandResult res = cmd.execute();
+            writer.writeln(res.getMessage());
+
+        }
+        catch (RuntimeException e){
+            writer.writeln("error: " + e.getMessage());
+        }
+        catch (IOException e) {
+            readers.clear();
+            readers.add(defaultReader);
+            writer = defaultWriter;
+        }
+
+
         return false;
     }
 
