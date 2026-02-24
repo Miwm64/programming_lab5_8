020e68f602bf078b033eb789dd971b8396b14a3c
Miwm64 <mike.dobkes@gmail.com>
2026-02-24 22:39:35 +0300

    Parameter default value deleted

--- src/main/java/ru/spb/miwm64/moviemanager/commands/AddCommand.java
+++ src/main/java/ru/spb/miwm64/moviemanager/commands/AddCommand.java
@@ -1,4 +1,182 @@
 package ru.spb.miwm64.moviemanager.commands;
 
-public class AddCommand {
+import ru.spb.miwm64.moviemanager.collectionmanager.CollectionManager;
+import ru.spb.miwm64.moviemanager.command.*;
+import ru.spb.miwm64.moviemanager.entities.*;
+
+import java.time.ZonedDateTime;
+
+public class AddCommand extends AbstractCommand {
+    private CollectionManager collectionManager;
+    public AddCommand(CollectionManager collectionManager){
+        this.collectionManager = collectionManager;
+        this.name = "add";
+        this.help = "add - add new element to collection";
+
+
+        // name - String, cannot be null or empty
+        Parameter<String> nameParam = new Parameter<>(
+                "name",
+                "Enter movie name",
+                s -> s,
+                s -> s != null && !s.trim().isEmpty(),
+                true
+        );
+
+        // coordinates.x - float, max 274
+        Parameter<Float> coordXParam = new Parameter<>(
+                "coordX",
+                "Enter coordinates x (float, max 274)",
+                Float::parseFloat,
+                x -> x <= 274,
+                true
+        );
+
+        // coordinates.y - Long, > -559, cannot be null
+        Parameter<Long> coordYParam = new Parameter<>(
+                "coordY",
+                "Enter coordinates y (Long, > -559)",
+                Long::parseLong,
+                y -> y > -559,
+                true
+        );
+
+
+        // oscarsCount - int, > 0
+        Parameter<Integer> oscarsParam = new Parameter<>(
+                "oscarsCount",
+                "Enter oscars count (int > 0)",
+                Integer::parseInt,
+                count -> count > 0,
+                true
+        );
+
+        // goldenPalmCount - long, > 0
+        Parameter<Long> goldenPalmParam = new Parameter<>(
+                "goldenPalmCount",
+                "Enter golden palm count (long > 0)",
+                Long::parseLong,
+                count -> count > 0,
+                true
+        );
+
+        // genre - MovieGenre enum, can be null
+        Parameter<MovieGenre> genreParam = new Parameter<>(
+                "genre",
+                "Enter genre (DRAMA/MUSICAL/TRAGEDY/THRILLER) or leave empty",
+                s -> MovieGenre.valueOf(s.toUpperCase()),
+                genre -> true,
+                false
+        );
+
+        // mpaaRating - MpaaRating enum, cannot be null
+        Parameter<MpaaRating> mpaaParam = new Parameter<>(
+                "mpaaRating",
+                "Enter mpaa rating (PG_13/R/NC_17)",
+                s -> MpaaRating.valueOf(s.toUpperCase().replace("-", "")),
+                rating -> true,
+                true
+        );
+
+        // operator.name - String, cannot be null or empty
+        Parameter<String> operatorNameParam = new Parameter<>(
+                "operatorName",
+                "Enter operator name",
+                s -> s,
+                s -> s != null && !s.trim().isEmpty(),
+                false
+        );
+
+        // operator.weight - float, > 0
+        Parameter<Float> operatorWeightParam = new Parameter<>(
+                "operatorWeight",
+                "Enter operator weight (float > 0)",
+                Float::parseFloat,
+                w -> w > 0,
+                true
+        );
+
+        // operator.hairColor - Color enum, cannot be null
+        Parameter<Color> hairColorParam = new Parameter<>(
+                "hairColor",
+                "Enter hair color (GREEN/RED/YELLOW/ORANGE/BROWN)",
+                s -> Color.valueOf(s.toUpperCase()),
+                color -> true,
+                true
+        );
+
+        // operator.nationality - Country enum, cannot be null
+        Parameter<Country> nationalityParam = new Parameter<>(
+                "nationality",
+                "Enter nationality (UNITED_KINGDOM/CHINA/INDIA/ITALY/THAILAND)",
+                s -> Country.valueOf(s.toUpperCase()),
+                country -> true,
+                true
+        );
+
+        // Movie fields
+        addParam(nameParam);
+        addParam(coordXParam);
+        addParam(coordYParam);
+        addParam(oscarsParam);
+        addParam(goldenPalmParam);
+        addParam(genreParam);
+        addParam(mpaaParam);
+
+        // Person fields (operator)
+        addParam(operatorNameParam);
+        addParam(operatorWeightParam);
+        addParam(hairColorParam);
+        addParam(nationalityParam);
+    }
+
+    @Override
+    public CommandResult execute() {
+        try {
+            checkParams();
+
+            Coordinates coords = new Coordinates(
+                    getValue("coordX"),
+                    getValue("coordY")
+            );
+
+            Person operator = null;
+            if (params.get("operatorName").isSet()) {
+                operator = new Person(
+                        getValue("operatorName"),
+                        getValue("operatorWeight"),
+                        getValue("hairColor"),
+                        getValue("nationality")
+                );
+            }
+            System.out.println(getValue("mpaaRating").toString());
+            System.out.println(getValue("mpaaRating").getClass());
+            Movie movie = new Movie(
+                    null,
+                    getValue("name"),
+                    coords,
+                    ZonedDateTime.now(),
+                    getValue("oscarsCount"),
+                    getValue("goldenPalmCount"),
+                    MovieGenre.DRAMA,
+                    MpaaRating.PG_13,
+                    operator
+            );
+
+            collectionManager.append(movie);
+
+            return new CommandResultSuccess(
+                    movie,
+                    "Movie added successfully with ID: " + movie.getId()
+            );
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return new CommandResultFailure(e.getMessage());
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    protected <T> T getValue(String name) {
+        return (T) params.get(name).getValue();
+    }
 }
