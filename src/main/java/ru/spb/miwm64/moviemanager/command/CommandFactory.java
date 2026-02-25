2127ab7dbcb4d6e1d7c31ccbc80771a0fe82b809
Miwm64 <mike.dobkes@gmail.com>
2026-02-25 04:11:48 +0300

    RemoveGreater; CountByGoldenPalm

--- src/main/java/ru/spb/miwm64/moviemanager/command/CommandFactory.java
+++ src/main/java/ru/spb/miwm64/moviemanager/command/CommandFactory.java
@@ -26,11 +26,13 @@ public final class CommandFactory {
 
         register("show", () -> new ShowCommand(collectionManager));
         register("info", () -> new InfoCommand(collectionManager));
+        register("count_by_golden_palm_count", () -> new CountByGoldenPalmCountCommand(collectionManager));
         register("add", () -> new AddCommand(collectionManager));
         register("add_if_min", () -> new AddIfMinCommand(collectionManager));
         register("update_id", () -> new UpdateByIDCommand(collectionManager));
         register("remove_by_id", () -> new RemoveByIDCommand(collectionManager));
         register("remove_at", () -> new RemoveByIndexCommand(collectionManager));
+        register("remove_greater", () -> new RemoveGreaterCommand(collectionManager));
         register("clear", () -> new ClearCommand(collectionManager));
 
         register("load", () -> new LoadCommand(collectionManager, xmlParser));
