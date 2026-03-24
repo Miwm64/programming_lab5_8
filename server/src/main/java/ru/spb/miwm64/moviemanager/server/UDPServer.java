29270131eedd6f08b964e5650b70c28b7eeddba2
Miwm64 <mike.dobkes@gmail.com>
2026-03-25 01:06:17 +0300

    UDPserver create

--- server/src/main/java/ru/spb/miwm64/moviemanager/server/UDPServer.java
+++ server/src/main/java/ru/spb/miwm64/moviemanager/server/UDPServer.java
@@ -1,4 +1,22 @@
 package ru.spb.miwm64.moviemanager.server;
 
+import ru.spb.miwm64.moviemanager.common.collection.CollectionManager;
+import java.io.IOException;
+
+
 public class UDPServer {
+
+    public UDPServer(int port, CollectionManager collectionManager) throws IOException {
+
+    }
+/*
+    public void run() {}
+    public void stop() {}
+    private void handlePacket(){}
+    class router{}
+    private void sendSuccessResponse(){}
+    private void sendErrorResponse(){}
+    array clients
+    class ClientState
+ */
 }
