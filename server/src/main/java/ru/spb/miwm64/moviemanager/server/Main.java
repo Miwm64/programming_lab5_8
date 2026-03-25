c35924ebd15007bbc519128567c9059c79b58733
Miwm64 <mike.dobkes@gmail.com>
2026-03-25 03:39:08 +0300

    Stream collection manager

--- server/src/main/java/ru/spb/miwm64/moviemanager/server/Main.java
+++ server/src/main/java/ru/spb/miwm64/moviemanager/server/Main.java
@@ -1,13 +1,13 @@
 package ru.spb.miwm64.moviemanager.server;
 
-import ru.spb.miwm64.moviemanager.server.collectionmanager.SortedCollectionManager;
+import ru.spb.miwm64.moviemanager.server.collectionmanager.StreamCollectionManager;
 import ru.spb.miwm64.moviemanager.server.net.UDPServer;
 
 public class Main {
     public static void main(String[] args) {
         UDPServer udpServer;
         try {
-            udpServer = new UDPServer(7878, new SortedCollectionManager());
+            udpServer = new UDPServer(7878, new StreamCollectionManager());
             udpServer.run();
         }
         catch (Exception e){}
