54fa8dc6211bcf0a354fd484e6a279701316b5d2
Miwm64 <mike.dobkes@gmail.com>
2026-03-25 03:10:19 +0300

    UDP transport class

---
+++ server/src/main/java/ru/spb/miwm64/moviemanager/server/UDPTransport.java
@@ -0,0 +1,33 @@
+package ru.spb.miwm64.moviemanager.server;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.DatagramChannel;
+
+public class UDPTransport {
+    private final DatagramChannel channel;
+
+    public UDPTransport(int port) throws IOException {
+        channel = DatagramChannel.open();
+        channel.bind(new InetSocketAddress(port));
+        channel.configureBlocking(false);
+    }
+
+    public SocketAddress receive(ByteBuffer buffer) throws IOException {
+        return channel.receive(buffer);
+    }
+
+    public void send(SocketAddress client, byte[] data) throws IOException {
+        channel.send(ByteBuffer.wrap(data), client);
+    }
+
+    public DatagramChannel getChannel() {
+        return channel;
+    }
+
+    public void close() throws IOException {
+        channel.close();
+    }
+}
