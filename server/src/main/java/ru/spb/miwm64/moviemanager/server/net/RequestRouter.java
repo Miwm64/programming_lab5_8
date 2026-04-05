560c1935abb773ba32fac23dd8b6d85e6aa79c8b
Miwm64 <mike.dobkes@gmail.com>
2026-04-05 20:31:43 +0300

    Hardcoded sync test value

--- server/src/main/java/ru/spb/miwm64/moviemanager/server/net/RequestRouter.java
+++ server/src/main/java/ru/spb/miwm64/moviemanager/server/net/RequestRouter.java
@@ -1,17 +1,19 @@
 package ru.spb.miwm64.moviemanager.server.net;
 
 import ru.spb.miwm64.moviemanager.common.collection.CollectionManager;
-import ru.spb.miwm64.moviemanager.common.entities.Movie;
-import ru.spb.miwm64.moviemanager.common.entities.Person;
+import ru.spb.miwm64.moviemanager.common.entities.*;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import ru.spb.miwm64.moviemanager.common.exceptions.NonExistentCommand;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ru.spb.miwm64.moviemanager.common.net.Batch;
+import ru.spb.miwm64.moviemanager.common.net.VersionedObject;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.util.*;
 
 public class RequestRouter {
     private final Map<String, Handler> handlers = new HashMap<>();
@@ -69,6 +71,39 @@ public class RequestRouter {
                 collectionManager.filterGreaterThanOperatorCommand(mapper.treeToValue(params, Person.class)));
         handlers.put("printFieldAscendingGoldenPalmCount", params ->
                 collectionManager.printFieldAscendingGoldenPalmCountCommand());
+        handlers.put("sync", params -> {
+            // Create operator Person objects
+            Person tarantino = new Person("Quentin Tarantino", 85, Color.BROWN, Country.INDIA);
+            Person gerwig = new Person("Greta Gerwig", 85, Color.BROWN, Country.INDIA);
+            Person bong = new Person("Bong Joon-ho", 85, Color.BROWN, Country.INDIA);
+
+            // Create movies
+            Movie updatedMovie = new Movie(1L, "Updated Movie Title", new Coordinates(1, 2l),
+                    ZonedDateTime.now(), 1, 1, MovieGenre.DRAMA, MpaaRating.R, tarantino);
+            Movie newMovieA = new Movie(2L, "New Movie A", new Coordinates(1, 2l),
+                    ZonedDateTime.now(), 1, 1, MovieGenre.DRAMA, MpaaRating.R, gerwig);
+            Movie newMovieB = new Movie(3L, "New Movie B", new Coordinates(1, 2l),
+                    ZonedDateTime.now(), 1, 1, MovieGenre.DRAMA, MpaaRating.R, bong);
+
+            // Wrap in VersionedObject with version numbers
+            VersionedObject<Movie> updateVO = new VersionedObject<>(5, updatedMovie);
+            VersionedObject<Movie> createVO_A = new VersionedObject<>(1, newMovieA);
+            VersionedObject<Movie> createVO_B = new VersionedObject<>(1, newMovieB);
+
+// Build the lists
+            ArrayList<VersionedObject<Movie>> updates = new ArrayList<>(Collections.singletonList(updateVO));
+            ArrayList<VersionedObject<Movie>> creates = new ArrayList<>(Arrays.asList(createVO_A, createVO_B));
+            ArrayList<Long> deletes = new ArrayList<>(Collections.singletonList(4L));
+            ArrayList<String> messages = new ArrayList<>(Arrays.asList(
+                    "Update for movie 1 applied successfully",
+                    "Movie 2 created",
+                    "Movie 3 created",
+                    "Movie 4 deleted"
+            ));
+
+// Return the Batch object
+            return new Batch(updates, creates, deletes, messages);
+        });
 
         LOG.debug("Handlers registered: {}", handlers.keySet());
     }
