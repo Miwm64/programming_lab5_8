ba262ae7094ed1f527c0bf62ab2af52b32ef91d2
Miwm64 <mike.dobkes@gmail.com>
2026-05-05 23:37:47 +0300

    Fixed ports

--- scripts/db.sh
+++ scripts/db.sh
@@ -2,4 +2,4 @@
 docker pull postgres:16
 docker stop moviemanager-postgres
 docker rm moviemanager-postgres
-docker run --name moviemanager-postgres --env-file .env -p 5433:5433/tcp -d postgres:16
+docker run --name moviemanager-postgres --env-file .env -p 5433:5432/tcp -d postgres:16
