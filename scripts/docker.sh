c81630b8a99a0c425e88fd8619af6b26a738db15
Miwm64 <mike.dobkes@gmail.com>
2026-05-10 19:21:03 +0300

    Docker compose

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -2,4 +2,4 @@
 docker build -t moviemanager-server .
 docker stop moviemanager-server
 docker rm moviemanager-server
-docker run --network moviemanager-net -it -v ./docker:/app/config -v ./logs:/app/logs --env-file .env_server  -p  7878:7878/udp --name moviemanager-server moviemanager-server
+docker run --network moviemanager-net -it -v ./docker:/app/config -v ./logs:/app/logs --env-file .env_docker_server  -p  7878:7878/udp --name moviemanager-server moviemanager-server
