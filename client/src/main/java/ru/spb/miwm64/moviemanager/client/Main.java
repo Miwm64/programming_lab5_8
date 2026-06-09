7224c7e9e4561b99487117813481f20b7721027b
Miwm64 <102681774+Miwm64@users.noreply.github.com>
2026-06-09 14:05:16 +0300

    Revert "Logic integration (#52)" (#53)
    This reverts commit 25c25eea8f56ff4c934f8e09187596d646644af0.

--- client/src/main/java/ru/spb/miwm64/moviemanager/client/Main.java
+++ client/src/main/java/ru/spb/miwm64/moviemanager/client/Main.java
@@ -1,31 +1,30 @@
 package ru.spb.miwm64.moviemanager.client;
 
-import javafx.application.Application;
-import javafx.geometry.Rectangle2D;
-import javafx.scene.Scene;
-import javafx.stage.Screen;
-import javafx.stage.Stage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.spb.miwm64.moviemanager.client.collectionmanager.BatchRemoteCollectionManager;
-import ru.spb.miwm64.moviemanager.client.command.CommandFactory;
-import ru.spb.miwm64.moviemanager.client.gui.MyScene;
-import ru.spb.miwm64.moviemanager.client.io.ConsoleReader;
-import ru.spb.miwm64.moviemanager.client.io.ConsoleWriter;
+import ru.spb.miwm64.moviemanager.common.collection.CollectionManager;
+import ru.spb.miwm64.moviemanager.client.collectionmanager.RemoteCollectionManager;
+import ru.spb.miwm64.moviemanager.client.io.*;
 import ru.spb.miwm64.moviemanager.client.net.JsonRpcClient;
 import ru.spb.miwm64.moviemanager.client.net.UDPClient;
+import ru.spb.miwm64.moviemanager.client.io.ConsoleReader;
 import ru.spb.miwm64.moviemanager.common.io.Reader;
 import ru.spb.miwm64.moviemanager.common.io.Writer;
 import ru.spb.miwm64.moviemanager.common.io.XMLParser;
-import ru.spb.miwm64.moviemanager.client.sync.*;
-import java.net.InetSocketAddress;
-import java.util.*;
 
-public class Main extends Application {
-    @Override
-    public void start(Stage primaryStage) {
+import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static java.lang.Thread.sleep;
+
+
+public class Main {
+    public static void main(String[] args) {
         Logger log = LoggerFactory.getLogger(Main.class);
-        log.info("JavaFX application started");
+        log.info("Application started");
         UDPClient udpClient = new UDPClient(new InetSocketAddress("localhost", 7878));
         JsonRpcClient jsonRpcClient = new JsonRpcClient(udpClient);
 
@@ -33,31 +32,17 @@ public class Main extends Application {
         BatchRemoteCollectionManager collectionManager = new BatchRemoteCollectionManager(queue);
 
         XMLParser xmlParser = new XMLParser();
+        Reader reader = new ConsoleReader();
+        Writer writer = new ConsoleWriter();
         List<String> messages = Collections.synchronizedList(new ArrayList<String>());
-
-        Scene scene = new MyScene(primaryStage, collectionManager, xmlParser, jsonRpcClient);
-        stageInit(primaryStage, scene);
 
         SynchronizationThread thread = new SynchronizationThread(jsonRpcClient, queue, collectionManager, messages);
         thread.start();
 
+        var mainController = new MainController(collectionManager, reader, writer, xmlParser, messages, jsonRpcClient);
+        mainController.run();
+
+        thread.gracefulShutdown();
         return;
-    }
-
-    public void stageInit(Stage primaryStage, Scene scene) {
-        Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
-        primaryStage.setTitle("Movie manager");
-        primaryStage.setWidth(primaryScreenBounds.getWidth()/1.5);
-        primaryStage.setHeight(primaryScreenBounds.getHeight()/1.5);
-//        primaryStage.initStyle(StageStyle.UNDECORATED);
-
-        // scene creation
-        primaryStage.setResizable(true);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-
-    public static void main(String[] args) {
-        launch(args);
     }
 }
